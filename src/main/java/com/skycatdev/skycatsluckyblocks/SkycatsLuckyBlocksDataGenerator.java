70fd88c36fbc3917f4c81abc9ac1032b0f2a8648
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-10-10 20:09:56 -0700

    Initial commit

---
+++ src/main/java/com/skycatdev/skycatsluckyblocks/SkycatsLuckyBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package com.skycatdev.skycatsluckyblocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkycatsLuckyBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
