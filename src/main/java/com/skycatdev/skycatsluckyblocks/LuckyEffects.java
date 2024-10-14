66d7ca1df8be28621d69425a83eb817fe8ea647e
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-10-14 14:54:00 -0700

    A few new effects

--- src/main/java/com/skycatdev/skycatsluckyblocks/LuckyEffects.java
+++ src/main/java/com/skycatdev/skycatsluckyblocks/LuckyEffects.java
@@ -1,12 +1,18 @@
 package com.skycatdev.skycatsluckyblocks;
 
 import com.skycatdev.skycatsluckyblocks.impl.SimpleLuckyEffect;
+import com.skycatdev.skycatsluckyblocks.mixin.SaplingGeneratorMixin;
+import net.minecraft.block.SaplingGenerator;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 
 import static com.skycatdev.skycatsluckyblocks.SkycatsLuckyBlocks.LOGGER;
 import static com.skycatdev.skycatsluckyblocks.SkycatsLuckyBlocks.MOD_ID;
@@ -18,18 +24,33 @@ public class LuckyEffects {
     })
             .addPool(LuckyEffectPools.DEFAULT, 0.1)
             .build();
-    public static final SimpleLuckyEffect SPAWN_IRON_GOLEM = new SimpleLuckyEffect.Builder(Identifier.of(MOD_ID, "spawn_iron_golem"), (world, pos, state, player) -> {
+    public static final SimpleLuckyEffect SPAWN_IRON_GOLEM = new SimpleLuckyEffect.Builder(Identifier.of(MOD_ID, "spawn_iron_golem"), (world, pos, state, player) -> spawnIronGolem(world, pos, player, false))
+            .addPool(LuckyEffectPools.DEFAULT, 1)
+            .build();
+    public static final SimpleLuckyEffect SPAWN_ANGRY_IRON_GOLEM = new SimpleLuckyEffect.Builder(Identifier.of(MOD_ID, "spawn_angry_iron_golem"), (world, pos, state, player) -> spawnIronGolem(world, pos, player, true))
+            .addPool(LuckyEffectPools.DEFAULT, 1)
+            .build();
+    public static final SimpleLuckyEffect RANDOM_TREE = new SimpleLuckyEffect.Builder(Identifier.of(MOD_ID, "random_tree"), ((world, pos, state, player) -> {
+        SaplingGenerator[] generators = SaplingGeneratorMixin.getGENERATORS().values().toArray(new SaplingGenerator[0]);
+        Random random = player.getRandom();
+        return generators[random.nextInt(generators.length)].generate(world, world.getChunkManager().getChunkGenerator(), pos, state, random);
+    }))
+            .addPool(LuckyEffectPools.DEFAULT, 1)
+            .build();
+
+    private static boolean spawnIronGolem(ServerWorld world, BlockPos pos, ServerPlayerEntity player, boolean angry) {
         IronGolemEntity golem = EntityType.IRON_GOLEM.spawn(world, pos, SpawnReason.COMMAND);
         if (golem != null) {
             golem.playSound(SoundEvents.ENTITY_IRON_GOLEM_REPAIR);
+            if (angry) {
+                golem.setAngryAt(player.getUuid());
+            }
             return true;
         } else {
             LOGGER.warn("Couldn't spawn a golem for spawn_iron_golem effect");
             return false;
         }
-    })
-            .addPool(LuckyEffectPools.DEFAULT, 1)
-            .build();
+    }
 
     public static void init() {
 
---
+++ src/main/java/com/skycatdev/skycatsluckyblocks/mixin/SaplingGeneratorMixin.java
@@ -0,0 +1,15 @@
+package com.skycatdev.skycatsluckyblocks.mixin;
+
+import net.minecraft.block.SaplingGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(SaplingGenerator.class)
+public interface SaplingGeneratorMixin { // TODO: Test with connector
+    @Accessor
+    static Map<String, SaplingGenerator> getGENERATORS() {
+        throw new AssertionError("Mixin does the work here");
+    }
+}
