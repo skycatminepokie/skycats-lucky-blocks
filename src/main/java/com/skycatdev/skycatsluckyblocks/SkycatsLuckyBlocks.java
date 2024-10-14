ce7c5436ddc1656afe50a2cb996546735e9b570c
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-10-14 14:53:54 -0700

    Use PlayerBlockBreakEvents.After for triggering lucky blocks

--- src/main/java/com/skycatdev/skycatsluckyblocks/SkycatsLuckyBlocks.java
+++ src/main/java/com/skycatdev/skycatsluckyblocks/SkycatsLuckyBlocks.java
@@ -1,18 +1,34 @@
 package com.skycatdev.skycatsluckyblocks;
 
+import com.skycatdev.skycatsluckyblocks.api.AbstractLuckyBlock;
 import com.skycatdev.skycatsluckyblocks.impl.LuckyBlocks;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SkycatsLuckyBlocks implements ModInitializer {
+public class SkycatsLuckyBlocks implements ModInitializer, PlayerBlockBreakEvents.After {
 	public static final String MOD_ID = "skycats-lucky-blocks";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
+	public void afterBlockBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity) {
+		if (state.getBlock() instanceof AbstractLuckyBlock luckyBlock) {
+			luckyBlock.afterBlockBreak(world, player, pos, state, blockEntity);
+		}
+	}
+
+	@Override
 	public void onInitialize() {
 		LuckyBlocks.init();
 		LuckyEffects.init();
+		PlayerBlockBreakEvents.AFTER.register(this);
 	}
 }
--- src/main/java/com/skycatdev/skycatsluckyblocks/api/AbstractLuckyBlock.java
+++ src/main/java/com/skycatdev/skycatsluckyblocks/api/AbstractLuckyBlock.java
@@ -1,23 +1,40 @@
 package com.skycatdev.skycatsluckyblocks.api;
 
 import com.skycatdev.skycatsluckyblocks.SkycatsLuckyBlocks;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public abstract class AbstractLuckyBlock extends Block {
+public abstract class AbstractLuckyBlock extends Block implements PlayerBlockBreakEvents.After {
     public static final int MAX_ATTEMPTS = 10;
+
     public AbstractLuckyBlock(Settings settings) {
         super(settings);
     }
 
+    /**
+     * Executes a {@link LuckyEffect} from this block's {@link LuckyEffectPool}.
+     *
+     * @param world       the world where the block was broken
+     * @param player      the player who broke the block
+     * @param pos         the position where the block was broken
+     * @param state       the block state <strong>before</strong> the block was broken
+     * @param blockEntity the block entity of the broken block, can be {@code null}
+     * @implSpec Do not register this, skycats-lucky-blocks will make sure it is called.
+     * @implNote {@link Block#onBroken}, {@link Block#onStateReplaced}, and {@link Block#onStacksDropped} aren't used because they don't provide a player.</br>
+     * {@link Block#onBreak} isn't used because the block is destroyed afterward, making any block placed at {@code pos} by the effect disappear.</br>
+     * This is filtered to only be on the server-side, since that's where all effects should be taking place. Anything client-side will need a packet - the server likely doesn't share the player's {@code Random}, so we can't rely on that.
+     */
     @Override
-    public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+    public void afterBlockBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity) {
         if (world instanceof ServerWorld serverWorld) {
             if (player instanceof ServerPlayerEntity serverPlayer) {
                 Random random = player.getRandom();
@@ -35,7 +52,6 @@ public abstract class AbstractLuckyBlock extends Block {
                 SkycatsLuckyBlocks.LOGGER.error("world was a ServerWorld, but player was not a ServerPlayerEntity? This is a bug, please report it at https://github.com/skycatminepokie/skycats-lucky-blocks/issues");
             }
         }
-        return super.onBreak(world, pos, state, player);
     }
 
     public abstract LuckyEffectPool getEffectPool();
